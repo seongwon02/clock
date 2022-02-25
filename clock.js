176c07af32eb2e508c7527d623e39157bc87ee4e
Seongwon Kim <seongwon2303@gmail.com>
2022-02-25 17:38:38 +0900

    second commit

--- clock.js
+++ clock.js
@@ -1,15 +1,60 @@
-function getTime(){
-    const time = new Date();
-    const hour = time.getHours();
-    const minute = time.getMinutes();
-    const second = time.getSeconds();
-    clock.innerHTML = `${hour<10 ? `0${hour}` : hour}:${minute<10 ? `0${minute}` : minute}:${second<10 ? `0${second}` : second}`
-}
+var Flipper = function() {
+    function Flipper(node, currentTime, nextTime) {
+        this.isFlipping = false;
+        this.duration = 600;
+        this.flipNode = node;
+        this.frontNode = node.querySelector(".front");
+        this.backNode = node.querySelector(".back");
+        this.setFrontTime(currentTime);
+        this.setBackTime(nextTime);
+    }
+    Flipper.prototype.setFrontTime  = function(time) {
+        this.frontNode.dataset.number = time;
+    };
+    Flipper.prototype.setBackTime = function (time) {
+        this.backNode.dataset.number = time;
+    };
+    Flipper.prototype.flipDown = function(currentTime, nextTime) {
+        var _this = this;
 
-function init(){
-    setInterval(getTime, 1000);
-}
+        if (this.isFlipping) {
+            return false;
+        }
 
-var clock = document.querySelector(".clock");
+        this.isFlipping = true;
+        this.setFrontTime(currentTime);
+        this.setBackTime(nextTime);
+        this.flipNode.classList.add("running");
+        setTimeout(function() {
+            _this.flipNode.classList.remove("running");
+            _this.isFlipping = false;
+            _this.setFrontTime(nextTime);
+        }, this.duration);
+    };
+    return Flipper;
+}();
 
-init();
+var getTimeFromDate = function(date) {
+    return date.toTimeString().slice(0, 8).split(":").join("");
+};
+
+var flips = document.querySelectorAll(".flip");
+var now = new Date();
+var nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+var nextTimeStr = getTimeFromDate(now);
+var Flippers = Array.from(flips).map(function (flip, i) {
+    return new Flipper(flip, nowTimeStr[i], nextTimeStr[i]);
+});
+
+setInterval(function () {
+    var now = new Date();
+    var nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+    var nextTimeStr = getTimeFromDate(now);
+
+    for (var i = 0; i < Flippers.length; i++) {
+        if (nowTimeStr[i] === nextTimeStr[i]) {
+            continue;
+        }
+        Flippers[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
+    }
+}, 1000);
